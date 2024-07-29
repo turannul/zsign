63ddf53e6b5631d3e1f0fb873b9b8e06eb79c104
Turann_ <84424530+turannul@users.noreply.github.com>
2024-07-29 11:01:03 +0300

    Cleaning up structure 4/4

--- INSTALL.sh
+++ INSTALL.sh
@@ -1,63 +1,51 @@
 #!/bin/bash
 
-MacOs=darwin
-CentOs=yum
-Ubuntu=apt-get
-CmakeV=3.21.3
+# Detect OS if it is macOS
+if [[ "$(uname)" == "Darwin" ]]; then
 
-# Compile on MacOs
+    # Install dependencies
+    brew install zip unzip openssl@1.1
 
-	# Detect Os if  it is MacOs 
-	if [[ "$OSTYPE" =~ ^$MacOs ]]; then
-		
-	# Dependencies 
-	brew install zip unzip &&
-	brew install openssl cmake  
+    # Get the installation prefix for OpenSSL 1.1
+    OPENSSL_PREFIX=$(brew --prefix openssl@1.1)
+    # Define the include and library paths
+    INCLUDE_PATH="$OPENSSL_PREFIX/include"
+    LIB_PATH="$OPENSSL_PREFIX/lib"
 
-# Compile zsign usign cmake
-mkdir build; cd build &&
-cmake .. && 
-make
+    echo "Include path: $INCLUDE_PATH"
+    echo "Library path: $LIB_PATH"
 
-
-# Compile on CentOS
-
-    # Detect OS if it is CentOS
-    elif [ -x /usr/bin/$CentOs ]; then
-
-        # Dependencies
-        yum install openssl-devel -y;
-        yum install wget zip unzip -y &&
-        yum group install "Development Tools" -y &&
-
-            # Installing Cmake latest
-wget -qO- "https://cmake.org/files/v3.21/cmake-$CmakeV-linux-x86_64.tar.gz" | \
-tar --strip-components=1 -xz -C /usr/local &&
-
-
-
-# Compile zsign using cmake
-mkdir build; cd build &&
-cmake .. &&
-make
-
-
-# Compile on Ubuntu
-
-    # Detect OS if it is ubuntu
-    elif [ -x /usr/bin/$Ubuntu ]; then
-
-
-        # Dependencies
-        sudo apt-get install wget zip unzip build-essential checkinstall zlib1g-dev libssl-dev -y &&
-
-            # Installing Cmake latest
-            wget -qO- "https://cmake.org/files/v3.21/cmake-$CmakeV-linux-x86_64.tar.gz" | \
-            tar --strip-components=1 -xz -C /usr/local &&
-
-
-# Compile zsign using cmake
-mkdir build; cd build &&
-cmake .. &&
-make
+    if [ -f build ]; then
+    # Remove old build directory
+        rm -rf build/
+    else
+    # Create build folder
+        mkdir -p build
     fi
+
+    # Compile
+    clang++ src/zsign/*.cpp -I"$INCLUDE_PATH" -L"$LIB_PATH" -lssl -lcrypto -O3 -o build/zsign
+
+else
+
+    # Dependencies for Linux
+    sudo apt-get update
+    sudo apt-get install wget zip unzip build-essential checkinstall zlib1g-dev libssl-dev -y
+    # OpenSSL Information
+    Linux_Include_Path=$(pkg-config --cflags openssl)
+    Linux_Lib_Path=$(pkg-config --libs openssl)
+
+    echo "Include path: $Linux_Include_Path"
+    echo "Library path: $Linux_Lib_Path"
+
+    if [ -f build ]; then
+    # Remove old build directory
+        rm -rf build/
+    else
+    # Create build folder
+        mkdir -p build
+    fi
+
+    # Compile
+    g++ src/zsign/*.cpp -I"$Linux_Include_Path" -L"$Linux_Lib_Path" -lssl -lcrypto -O3 -o build/zsign
+fi
