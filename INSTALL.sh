5177705a77cbce519e7cd8e11b73f5b8db5edc54
Turann_ <84424530+turannul@users.noreply.github.com>
2024-08-03 04:31:54 +0300

    Update INSTALL.sh

--- INSTALL.sh
+++ INSTALL.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-build() {
+create_Dir() {
+
     if [ -d build ]; then
         echo "Removing old build directory..."
         rm -rf build/
@@ -8,7 +9,10 @@ build() {
 
     echo "Creating build directory"
     mkdir -p build
-    echo "Building..."
+}
+compile() {
+    create_Dir
+    echo "Compiling..."
     cd build && cmake .. && make
 }
 
@@ -17,20 +21,16 @@ if [[ "$(uname)" == "Darwin" ]]; then
     brew install zip unzip openssl@1.1 -q || { echo "Failed to install macOS dependencies"; exit 1; }
     build
 elif command -v brew > /dev/null; then
-    echo "Homebrew found at: $(brew --prefix)"
-    echo "Using Linux Brew to install dependencies..."
-    brew update
-    brew install cmake zip unzip make gcc zlib openssl@1.1 pkg-config || { echo "Failed to install Linux Brew dependencies"; exit 1; }
-    sudo apt-get install -y checkinstall
-    # export LDFLAGS="-L/usr/local/opt/openssl@1.1/lib"
-    # export CPPFLAGS="-I/usr/local/opt/openssl@1.1/include"
-    build
-elif command -v apt-get > /dev/null; then
     echo "Updating and installing Linux (with apt) dependencies..."
     sudo apt-get update
     sudo apt-get install -y zip unzip build-essential checkinstall zlib1g-dev libssl-dev pkg-config || { echo "Failed to install Linux dependencies"; exit 1; }
-    build
+    brew update
+    brew install openssl@1.1 || { echo "Failed to install OpenSSL@1.1"; exit 2; }
+    ssl_lib="-L$(brew --prefix openssl@1.1)/*/lib"
+    ssl_incl="-I$(brew --prefix openssl@1.1)/*/include"
+    create_Dir
+    g++ src/zsign/*.cpp -lcrypto "${ssl_incl}" "${ssl_lib}" -O3 -o build/zsign || { echo "Build failed"; exit 2; }
 else
-    echo "Unsupported."
-    exit 1
+    echo "Homebrew is required for openssl@1.1."
+    exit 4
 fi
