6fd121639837ae1dbe42f194d8ef461e7a35eb78
Turann_ <84424530+turannul@users.noreply.github.com>
2024-08-03 03:57:38 +0300

    Update INSTALL.sh

--- INSTALL.sh
+++ INSTALL.sh
@@ -16,8 +16,20 @@ if [[ "$(uname)" == "Darwin" ]]; then
     echo "Installing macOS dependencies..."
     brew install zip unzip openssl@1.1 -q || { echo "Failed to install macOS dependencies"; exit 1; }
     build
-else
-    echo "Updating and installing Linux dependencies..."
-    sudo apt-get update && sudo apt-get install -y zip unzip build-essential checkinstall zlib1g-dev libssl-dev pkg-config || { echo "Failed to install Linux dependencies"; exit 1; }
+elif command -v brew > /dev/null; then
+    echo "Linux Brew found at: $(brew --prefix)"
+    echo "Using Linux Brew to install dependencies..."
+    brew update
+    brew install zip unzip make gcc checkinstall zlib openssl@1.1 pkg-config || { echo "Failed to install Linux Brew dependencies"; exit 1; }
+    # export LDFLAGS="-L/usr/local/opt/openssl@1.1/lib"
+    # export CPPFLAGS="-I/usr/local/opt/openssl@1.1/include"
     build
+elif command -v apt-get > /dev/null; then
+    echo "Updating and installing Linux (with apt) dependencies..."
+    sudo apt-get update
+    sudo apt-get install -y zip unzip build-essential checkinstall zlib1g-dev libssl-dev pkg-config || { echo "Failed to install Linux dependencies"; exit 1; }
+    build
+else
+    echo "Unsupported."
+    exit 1
 fi
