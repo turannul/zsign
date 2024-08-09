6d3585791d4ff015bcbcf937ad7c6900e321e8bd
Turann_ <84424530+turannul@users.noreply.github.com>
2024-08-10 00:29:33 +0300

    fix #?@$!$

--- CTestTestfile.cmake
+++ CTestTestfile.cmake
@@ -1,18 +1,28 @@
-find_program(ZSIGN_EXECUTABLE zsign PATHS /usr/local/bin)
-message("zsign found at: ${ZSIGN_EXECUTABLE}")
+find_program(zsign_exec zsign PATHS /usr/local/bin)
+find_file(encrypted_p12 NAMES Encrypted.p12 PATHS Test)
+find_file(unencrypted_p12 NAMES Unencrypted.p12 PATHS Test)
+find_file(provision_profile NAMES test.mobileprovision PATHS Test)
+find_file(ipa NAMES test.ipa PATHS Test)
+
+message("zsign found: ${zsign_exec}")
+message("Encrypted .p12 found: ${encrypted_p12}")
+message("Unencrypted .p12 found: ${unencrypted_p12}")
+message("Provision profile found: ${provision_profile}")
+message("iPA found: ${ipa}")
+
 
 add_test(Exec_cmd
-    ${ZSIGN_EXECUTABLE} -v)
+    ${zsign_exec} -v)
 
 add_test(Encrypted_Cert
-    ${ZSIGN_EXECUTABLE} -k Test/Encrypted.p12
-            -m Test/test.mobileprovision
+    ${zsign_exec} -k ${encrypted_p12}
+            -m ${provision_profile}
             -p 1234 
-            Test/test.ipa
+            ${ipa}
             -o /tmp/successful_Test.ipa)
 
 add_test(Unencrypted_Cert
-    ${ZSIGN_EXECUTABLE} -k Test/Unencrypted.p12 
-            -m Test/test.mobileprovision 
-            Test/test.ipa 
+    ${zsign_exec} -k ${unencrypted_p12}
+            -m ${provision_profile}
+            ${ipa} 
             -o /tmp/successful_Test.ipa)
