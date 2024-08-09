21789989c9a625f7b552d61d4aa90c291f5502aa
Turann_ <84424530+turannul@users.noreply.github.com>
2024-08-10 00:43:39 +0300

    Update CTestTestfile.cmake

--- CTestTestfile.cmake
+++ CTestTestfile.cmake
@@ -20,9 +20,11 @@ add_test(Encrypted_Cert
             -p 1234 
             ${ipa}
             -o /tmp/successful_Test.ipa)
+            --force
 
 add_test(Unencrypted_Cert
     ${zsign_exec} -k ${unencrypted_p12}
             -m ${provision_profile}
             ${ipa} 
             -o /tmp/successful_Test.ipa)
+            --force
