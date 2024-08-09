4a93965ea018a0ae6ec514a3b1761b2930c794fb
Turann_ <84424530+turannul@users.noreply.github.com>
2024-08-09 23:36:43 +0300

    Update CTestTestfile.cmake

--- CTestTestfile.cmake
+++ CTestTestfile.cmake
@@ -1,15 +1,15 @@
 add_test(Exec_cmd
-    COMMAND build/zsign -v)
+    COMMAND zsign -v)
 
 add_test(Encrypted_Cert
-    COMMAND build/zsign -k Test/Encrypted.p12
+    COMMAND zsign -k Test/Encrypted.p12
             -m Test/test.mobileprovision
             -p 1234 
             Test/test.ipa
             -o /tmp/successful_Test.ipa)
 
 add_test(Unencrypted_Cert
-    COMMAND build/zsign -k Test/Unencrypted.p12 
+    COMMAND zsign -k Test/Unencrypted.p12 
             -m Test/test.mobileprovision 
             Test/test.ipa 
             -o /tmp/successful_Test.ipa)
